45ad03d9e9a280a3ba0be8f0c8d708cbba264227
Kehinde Onifade <onifkay@gmail.com>
2023-01-04 20:07:42 +0100

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'users/index'
+  get 'cleaners/index'
+  get 'appointments/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
