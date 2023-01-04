be9886c10889394758d601be17c75567134f5f24
Kehinde Onifade <onifkay@gmail.com>
2023-01-04 21:00:03 +0100

    update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
     def index
-        
+        @users = User.all
+
+        render json: @users
+    end
+
+    def show
+        render json: @user
     end
 end
