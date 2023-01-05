27c44e86f813730a0641cebbb61919efb489355e
Kehinde Onifade <onifkay@gmail.com>
2023-01-05 12:25:42 +0100

    generate devise controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
+  }
 
   namespace :api do
     namespace :v1 do
