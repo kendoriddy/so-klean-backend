028c85e1b617d1c608aac5024ef30d2bcb54c7f6
Kehinde Onifade <onifkay@gmail.com>
2023-01-10 12:11:45 +0100

    update user permission

--- app/controllers/api/v1/appointments_controller.rb
+++ app/controllers/api/v1/appointments_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::AppointmentsController < ApplicationController
   def index
-    @appointments = current_user.appointments
+    # @appointments = current_user.appointments
+    @appointments = Appointment.all
 
     render json: @appointments
   end
