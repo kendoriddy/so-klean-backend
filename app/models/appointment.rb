46b52ed006f914754029a8638df1595e531b6b98
Kehinde Onifade <onifkay@gmail.com>
2023-01-15 16:08:25 +0100

    add validation to appointments

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,6 +2,9 @@ class Appointment < ApplicationRecord
   belongs_to :user
   belongs_to :cleaner
 
+  validates :location, presence: true
+  validates :date, presence: true
+
   def self.of_customer(user)
     where(user_id: user.id)
   end
