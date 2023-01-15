0a6d4202d749e02194235b47ddd09baf045a8c57
Kehinde Onifade <onifkay@gmail.com>
2023-01-15 15:56:38 +0100

    Add validation to cleaner model

--- app/models/cleaner.rb
+++ app/models/cleaner.rb
@@ -1,4 +1,9 @@
 class Cleaner < ApplicationRecord
   has_many :appointments, dependent: :destroy
   has_many :users, through: :appointments
+
+  validates :name, presence: true
+  validates :location, presence: true
+  validates :charges, presence: true, numericality: { greater_than_or_equal_to: 1000, less_than_or_equal_to: 100000, message: "must be between 1000 and 100000" }
+  validates :photo, presence: true
 end
