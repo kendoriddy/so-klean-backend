dc1badeef99d8c8bcd8cc7b1d82bc45715f42c62
Kehinde Onifade <onifkay@gmail.com>
2023-01-16 09:31:31 +0100

    Add rswag for cleaners

--- app/models/cleaner.rb
+++ app/models/cleaner.rb
@@ -5,6 +5,6 @@ class Cleaner < ApplicationRecord
   validates :name, presence: true
   validates :location, presence: true
   validates :charges, presence: true,
-                      numericality: { greater_than_or_equal_to: 1000, less_than_or_equal_to: 100_000, message: 'must be between 1000 and 100000' }
+                      numericality: { greater_than_or_equal_to: 1000.0, less_than_or_equal_to: 100_000.0, message: 'must be between 1000 and 100000' }
   validates :photo, presence: true
 end
---
+++ spec/requests/api/v1/cleaners_spec.rb
@@ -0,0 +1,135 @@
+require 'swagger_helper'
+
+RSpec.describe 'api/v1/cleaners', type: :request do
+
+  path '/api/v1/cleaners' do
+
+    get('list cleaners') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    post('create cleaner') do
+      response(200, 'cleaner added successfully') do
+
+        consumes 'application/json'
+        parameter name: :cleaner, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string },
+          location: { type: :string },
+          charges: { type: :float },
+          photo: { type: :string }
+        },
+        required: %w[name location charges photo]
+        }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/api/v1/cleaners/{id}' do
+    parameter name: 'id', in: :path, type: :string, description: 'id'
+
+    get('show cleaner') do
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    patch('update cleaner') do
+      response(200, 'cleaner added successfully') do
+        let(:id) { '123' }
+
+        consumes 'application/json'
+        parameter name: :cleaner, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string },
+          location: { type: :string },
+          charges: { type: :integer },
+          photo: { type: :string }
+        },
+        required: %w[name location charges photo]
+        }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    put('update cleaner') do
+      response(200, 'cleaner added successfully') do
+        let(:id) { '123' }
+
+        consumes 'application/json'
+        parameter name: :cleaner, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string },
+          location: { type: :string },
+          charges: { type: :integer },
+          photo: { type: :string }
+        },
+        required: %w[name location charges photo]
+        }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    delete('delete cleaner') do
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end
