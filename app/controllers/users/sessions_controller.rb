f84b97cd7e0850b2085529c0c71821e2c96fbe6d
Kehinde Onifade <onifkay@gmail.com>
2023-01-16 12:01:37 +0100

    update signout function

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -24,22 +24,20 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   def respond_to_on_destroy
-    log_out_success && return if current_user
-
-    log_out_failure
+    authenticate_user!
+    log_out_success
   end
 
   def log_out_success
     render json: {
       message: 'You are logged out',
-      user: current_user
-    }, status: (:ok || :unauthorized)
+      status: 204
+    }, status: :ok
   end
 
   def log_out_failure
     render json: {
-      message: 'something went wrong',
-      status:
-    }, status: :unprocessable_entity
+      message: 'something went wrong'
+    }, status: :unauthorized
   end
 end
