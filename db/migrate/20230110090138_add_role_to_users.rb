b9eeca3a1cbee05b553d16c5943f8637bf91125b
Kehinde Onifade <onifkay@gmail.com>
2023-01-17 12:56:42 +0100

    Add default role to user

--- db/migrate/20230110090138_add_role_to_users.rb
+++ db/migrate/20230110090138_add_role_to_users.rb
@@ -1,5 +1,6 @@
 class AddRoleToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :role, :string
+    change_column_default :users, :role, from: nil, to: "customer"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -50,7 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_10_090138) do
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.string "jti", null: false
-    t.string "role"
+    t.string "role", default: "customer"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
